e880c0e096548b9253c2fb402c6371f5c5f6fd9d
ivanzzh <57381060+ivanzzh@users.noreply.github.com>
2025-07-25 16:45:43 -0400

    Update General_Reserovir_comparsion.py

--- General_Reserovir_comparsion.py
+++ General_Reserovir_comparsion.py
@@ -3,7 +3,7 @@ from utils import *
 from torch.utils.data import Dataset, DataLoader
 import os
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from deal import load_config
 import pandas as pd
@@ -39,9 +39,10 @@ epochs = 200
 is_decay_constant = True
 is_early_stop = True
 back_ratio = 0.1
-train_data_path = 'dataset/{}/train_data.npz'.format(name)
-test_data_path = 'dataset/{}/test_data.npz'.format(name)
-image_direct = 'dataset/{}/result/'.format(name)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+train_data_path = os.path.join(BASE_DIR, 'dataset', f'{name}', 'train_data.npz')
+test_data_path = os.path.join(BASE_DIR, 'dataset', f'{name}', 'test_data.npz', )
+image_direct = os.path.join(BASE_DIR, 'dataset', f'{name}', 'result')
 point_update_period = 3
 point_num = dataset_config['point_num']
 voltage_lambda = 0.5
@@ -329,14 +330,8 @@ def main():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
-    if data_split:
-        train_data = load_dataset(train_data_path, layer3_neuron_number)
-        test_data = load_dataset(test_data_path, layer3_neuron_number)
-    else:
-        all_data = load_dataset(all_data_path, layer3_neuron_number)
-        train_size = int(split_ratio * len(all_data))
-        test_size = len(all_data) - train_size
-        train_data, test_data = torch.utils.data.random_split(all_data, [train_size, test_size])
+    train_data = load_dataset(train_data_path, layer3_neuron_number)
+    test_data = load_dataset(test_data_path, layer3_neuron_number)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True, drop_last=True)
     acc_list = []
@@ -361,8 +356,7 @@ def main():
         Encoders.append('Reservoir {}'.format(i))
         Decoders.append('Decoder {}'.format(i))
 
-    Res = R_Sp_Link_SNN(input_size, r_output, r_state, batch_size, voltage_lambda, p=p_p, p_self=p_self,
-                        no_cycle=False, no_bias=no_bias, k_value=k_value, r_state_only=r_state_only)
+    Res = R_Sp_Link_SNN(input_size, r_output, r_state, batch_size, voltage_lambda, no_bias=no_bias, k_value=k_value, r_state_only=r_state_only)
     snn1 = ESNN(r_output, layer1_neuron_number, batch_size, voltage_lambda, is_decay_constant,
                 is_voltage_decay_constant=False, alpha=alpha, beta=beta)
     snn2 = ESNN(layer1_neuron_number, layer2_neuron_number, batch_size, voltage_lambda, is_decay_constant,
